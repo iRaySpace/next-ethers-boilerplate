88c3d2b2eab9eedc16d556c89a87b767f10d7555
Ivan Ray Altomera <irayspacii@gmail.com>
2021-03-04 16:12:51 +0800

    initial commit

---
+++ src/pages/app.tsx
@@ -0,0 +1,31 @@
+export default function App() {
+  return (
+    <div className="flex bg-gray-100">
+      <aside className="h-screen w-64 shadow-xl bg-gradient-to-br from-red-400 to-pink-500">
+        <header className="p-7">
+          <div className="uppercase text-center text-white">
+            <a href="#">Ethers App</a>
+          </div>
+        </header>
+        <nav className="pt-3">
+          <a className="flex py-5 pl-7 text-white bg-pink-700 cursor-pointer">
+            Home
+          </a>
+          <a className="flex py-5 pl-7 text-white cursor-pointer">
+            Transactions
+          </a>
+        </nav>
+      </aside>
+      <div className="w-full h-screen">
+        <header className="flex items-center bg-white py-5 px-7">
+          <div className="w-1/2"></div>
+          <div className="w-1/2 flex justify-end">
+            <button className="border border-red-400 rounded-xl text-red-400 p-3 hover:bg-red-400 hover:text-white transition">
+              Logout
+            </button>
+          </div>
+        </header>
+      </div>
+    </div>
+  )
+}
